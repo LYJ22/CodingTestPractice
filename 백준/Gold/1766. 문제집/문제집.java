20f976caba169f8f8f2cf52c4491714ecd8355cd
Lee YuJeong(이유정) <79090053+LYJ22@users.noreply.github.com>
2026-03-04 21:31:03 +0900

    [Gold II] Title: 문제집, Time: 1040 ms, Memory: 180452 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        List<Integer>[] from = new List[N+1];
+        int[] to = new int[N+1];
+        boolean[] visited = new boolean[N+1];
+        
+        for(int n=1; n<=N; n++) {
+        	from[n] = new ArrayList<>();
+        }
+        
+        for(int m=0; m<M; m++) {
+        	int tall = sc.nextInt();
+        	int small = sc.nextInt();
+        	from[tall].add(small);
+        	to[small]++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> start = new PriorityQueue<>();
+        for(int n=1; n<=N; n++) {	// 맨 앞에 서 있을 수 있는 경우
+        	if(to[n] == 0) start.add(n);
+        }
+        
+        
+        while(!start.isEmpty()) {
+        	int curNode = start.poll();
+        	sb.append(curNode).append(" ");
+        	visited[curNode] = true;
+        	
+        	for(int nextNode: from[curNode]) {        		
+        		to[nextNode]--;
+        		if(to[nextNode] == 0) start.add(nextNode);
+        	}
+        }
+        
+        for(int n=1; n<=N; n++) {
+        	if(!visited[n]) sb.append(n).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
